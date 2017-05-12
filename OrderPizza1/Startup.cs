fd5e3db10cfbf481a3c895a49ccbffa66a0db16a
mjohngladson <mjohngladson@gmail.com>
2017-05-12 19:43:41 +0530

    Add project files.

---
+++ OrderPizza1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrderPizza1.Startup))]
+namespace OrderPizza1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
